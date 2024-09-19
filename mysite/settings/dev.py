2c2c972db750638f88b66d3aa39f05b0339b3f0a
Mahbubur <mbubur@gmail.com>
2024-09-19 14:40:14 +0530

    READMR.md added

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-grlemry8uz2k!gs$kb!qzj0@b543g^ats)gqqxajgz%@@q1j+f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
